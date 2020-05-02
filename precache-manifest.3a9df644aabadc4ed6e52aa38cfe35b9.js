585196e29876c0bebfefd6cdf49db4b0cba2c207
Jayanta Kurumathur <jayanta.k@outlook.com>
2020-05-02 11:52:21 +0200

    Updates

---
+++ precache-manifest.3a9df644aabadc4ed6e52aa38cfe35b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6730fe021e986d71f4715867bc16a4f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1824af7f6e6b6ef58954",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4830b845da654fb75f5a",
+    "url": "/static/js/2.fa2440a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa2440a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1824af7f6e6b6ef58954",
+    "url": "/static/js/main.f6ea92fa.chunk.js"
+  },
+  {
+    "revision": "ab2b4f2e2c7b56903ba8",
+    "url": "/static/js/runtime-main.eb8338c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
